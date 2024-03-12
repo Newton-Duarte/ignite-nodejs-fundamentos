fbc064ceb76bdc5c37088590bbc3af913ed46d03
Newton Duarte <newton_@hotmail.com>
2024-03-12 11:34:19 -0300

    feat: add users routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,33 @@
 import http from 'node:http';
 
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+  }
+]
+
 const server = http.createServer((request, response) => {
-  return response.end('Hello World')
+  const { method, url } = request
+
+  if (method === 'GET' && url === '/users') {
+    return response
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
+    return response.writeHead(201).end()
+  }
+
+  return response.writeHead(404).end()
 })
 
 server.listen(3333, () => console.log('Server running on por 3333!'));
